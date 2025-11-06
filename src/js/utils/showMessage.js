2d156bc6526cc19886a3d820da720f7913160c98
kushal <kushal@itobuz.com>
2025-11-06 16:27:28 +0530

    fix: show message

--- src/js/utils/showMessage.js
+++ src/js/utils/showMessage.js
@@ -2,15 +2,26 @@ import * as bootstrap from 'bootstrap';
 
 function showMessage(text, type = 'info', messageId = 'message') {
   const messageDiv = document.getElementById(messageId);
-  messageDiv.textContent = text;
   if (type === 'danger') {
-    messageDiv.style.color = 'red';
+    messageDiv.innerHTML = `<div
+      class="p-4 mt-2.5 text-sm text-red-800 rounded-lg bg-red-100" role="alert">
+      ${text}
+    </div>`;
   } else if (type === 'info') {
-    messageDiv.style.color = 'blue';
+    messageDiv.innerHTML = `<div
+      class="p-4 mt-2.5 text-sm text-blue-800 rounded-lg bg-blue-100" role="alert">
+      ${text}
+    </div>`;
   } else if (type === 'success') {
-    messageDiv.style.color = 'MediumSpringGreen';
+    messageDiv.innerHTML = `<div
+      class="p-4 mt-2.5 text-sm text-green-800 rounded-lg bg-green-100" role="alert">
+      ${text}
+    </div>`;
   } else if (type === 'warning') {
-    messageDiv.style.color = 'crimson';
+    messageDiv.innerHTML = `<div 
+    class="p-4 mt-2.5 text-sm text-yellow-800 rounded-lg bg-yellow-100" role="alert">
+  ${text}
+</div>`;
   }
 }
 
