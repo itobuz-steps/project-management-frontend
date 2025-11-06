44288b7ddb5cd1840bf1f2e12beafc8e5afa2647
Sohan Chatterjee <sohan@itobuz.com>
2025-11-06 18:20:03 +0530

    PM-63 chore: dropdown button rotation added with js no major changes

--- src/js/pages/dashboard/dashboard.js
+++ src/js/pages/dashboard/dashboard.js
@@ -1,15 +1,31 @@
 import '../../../scss/main.css';
 
-document
-  .getElementById('dropdownButtonForSprint')
-  .addEventListener('click', function () {
-    const dropdownMenu = document.querySelector('.dropdown-menu-sprint');
-    dropdownMenu.classList.toggle('hidden');
-  });
+const dropdownButtonSprint = document.getElementById('dropdownButtonForSprint');
+dropdownButtonSprint.addEventListener('click', function () {
+  const dropdownMenu = document.querySelector('.dropdown-menu-sprint');
+  dropdownMenu.classList.toggle('hidden');
+});
 
-document
-  .getElementById('dropdownButtonForBacklog')
-  .addEventListener('click', function () {
-    const dropdownMenu = document.querySelector('.dropdown-menu-backlog');
-    dropdownMenu.classList.toggle('hidden');
-  });
+const dropdownButtonBacklog = document.getElementById('dropdownButtonForBacklog');
+dropdownButtonBacklog.addEventListener('click', function () {
+  const dropdownMenu = document.querySelector('.dropdown-menu-backlog');
+  dropdownMenu.classList.toggle('hidden');
+});
+
+const dropdownIconSprint = document.getElementById('dropdown-icon-sprint');
+dropdownButtonSprint.addEventListener('click', function () {
+  if (dropdownIconSprint.classList.contains('rotate-270')) {
+    dropdownIconSprint.classList.remove('rotate-270');
+  } else {
+    dropdownIconSprint.classList.add('rotate-270');
+  }
+});
+
+const dropdownIconBacklog = document.getElementById('dropdown-icon-backlog');
+dropdownButtonBacklog.addEventListener('click', function () {
+  if (dropdownIconBacklog.classList.contains('rotate-270')) {
+    dropdownIconBacklog.classList.remove('rotate-270');
+  } else {
+    dropdownIconBacklog.classList.add('rotate-270');
+  }
+});
