cb436e989606089bdea392bbb4632d166c9ad82a
Sohan Chatterjee <sohan@itobuz.com>
2025-11-06 17:30:21 +0530

    PM-63 backlog and sprint table with drop down option created

--- src/js/pages/dashboard/dashboard.js
+++ src/js/pages/dashboard/dashboard.js
@@ -3,6 +3,13 @@ import '../../../scss/main.css';
 document
   .getElementById('dropdownButtonForSprint')
   .addEventListener('click', function () {
-    const dropdownMenu = document.querySelector('.dropdown-menu');
+    const dropdownMenu = document.querySelector('.dropdown-menu-sprint');
+    dropdownMenu.classList.toggle('hidden');
+  });
+
+document
+  .getElementById('dropdownButtonForBacklog')
+  .addEventListener('click', function () {
+    const dropdownMenu = document.querySelector('.dropdown-menu-backlog');
     dropdownMenu.classList.toggle('hidden');
   });
