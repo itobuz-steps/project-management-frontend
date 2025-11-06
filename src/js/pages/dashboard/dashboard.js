b18706e01b4226f3cc8ebe152403f9fb9a9f3010
Sohan Chatterjee <sohan@itobuz.com>
2025-11-06 17:02:31 +0530

    PM-63 sprint-view table created, also same design for list view created

--- src/js/pages/dashboard/dashboard.js
+++ src/js/pages/dashboard/dashboard.js
@@ -1,1 +1,8 @@
 import '../../../scss/main.css';
+
+document
+  .getElementById('dropdownButtonForSprint')
+  .addEventListener('click', function () {
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+    dropdownMenu.classList.toggle('hidden');
+  });
