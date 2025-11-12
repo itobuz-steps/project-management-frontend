b3a281ad23b764c9ebb2b642f00e33a640346824
Sohan Chatterjee <sohan@itobuz.com>
2025-11-12 11:12:36 +0530

    PM-85 fix: api url in project service

--- src/js/services/ProjectService.js
+++ src/js/services/ProjectService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001/projects';
+const API_BASE_URL = 'http://localhost:3001/project';
 
 class ProjectService {
   api = axios.create({
