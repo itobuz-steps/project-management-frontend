82175d3d0f8c2399d83e2d2f9cd8b6b24d29f1bf
kushal <kushal@itobuz.com>
2025-11-14 16:15:56 +0530

    PM-101 feat: edit profile page

---
+++ src/js/pages/editProfile/editProfile.js
@@ -0,0 +1,41 @@
+import '../../../scss/main.css';
+
+import authService from '../../services/AuthService.js';
+import { showMessage } from '../../utils/showMessage.js';
+
+async function editProfile() {
+  const form = document.getElementById('edit-profile-form');
+  const profileImage = document.getElementById('profileImageAdd');
+  const preview = document.getElementById('preview');
+  const userEmail = document.getElementById('user-email');
+  const name = document.getElementById('name');
+
+  const userInfo = await authService.getUserInfo();
+  console.log(userInfo);
+
+  if (userInfo.name) {
+    name.value = userInfo.name;
+  }
+
+  if (userInfo.profileImage) {
+    preview.src =
+      'http://localhost:3001/uploads/profile/' + userInfo.profileImage;
+  }
+  userEmail.innerText = userInfo.email;
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    try {
+      await authService.updateUserInfo(name.value, profileImage.files[0]);
+      showMessage('Profile Updated', 'success');
+
+      setTimeout(() => {
+        window.location.href = 'dashboard';
+      }, 1500);
+    } catch (err) {
+      console.log(err);
+      showMessage(`${err.response.data.error}`, 'danger');
+    }
+  });
+}
+
+editProfile();
