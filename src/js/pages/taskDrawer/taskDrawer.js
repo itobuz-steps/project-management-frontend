00e657aa35f9876ed4f5d64e35a992f7bcd9fc30
kushal <kushal@itobuz.com>
2025-12-11 17:24:36 +0530

    PM-191 subtasks bug fixes

--- src/js/pages/taskDrawer/taskDrawer.js
+++ src/js/pages/taskDrawer/taskDrawer.js
@@ -387,22 +387,28 @@ export async function showTaskDrawer(taskId) {
 
     const subtasks = all.filter((t) => task.subTask.includes(t._id));
 
-    subtasks.forEach((st) => {
+    subtasks.forEach(async (st) => {
+      console.log('sub tasks: ', st._id);
+
+      const subtaskAssignee = st.assignee
+        ? (await taskService.getUserDetailsById(st.assignee)).data.result
+        : null;
       const div = document.createElement('div');
       div.className =
         'flex items-start bg-white rounded-lg shadow-md pl-3 py-4 border border-[#90e0ef]';
       div.innerHTML = `
       <img
         src="${
-          assignee
-            ? assignee.profileImage
-              ? 'http://localhost:3001/uploads/profile/' + assignee.profileImage
+          subtaskAssignee
+            ? subtaskAssignee.profileImage
+              ? 'http://localhost:3001/uploads/profile/' +
+                subtaskAssignee.profileImage
               : '../../../assets/img/profile.png'
             : '../../../assets/img/profile.png'
         }"
          
         class="w-8 h-8 rounded-full border-2 border-[#00b4d8]"
-        title = "${assignee ? assignee.name : 'unassigned'}"
+        title = "${subtaskAssignee ? subtaskAssignee.name : 'unassigned'}"
       />
       <div class="ml-3">
         <span class="font-medium text-[#03045e] text-md">${st.title}</span>
